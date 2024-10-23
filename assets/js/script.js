de4e366f3683203a2305310a06c201386949b5b2
Tayla Ball <tayla.ball@hotmail.com>
2024-10-23 09:42:25 +0000

    Add: some js for generating a random number

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
-// Generate a random number between 1 and 10
-let randomNumber = Math.floor(Math.random() * 10) +1;
+// Assigning variables
+Let score = 0;
+
+// Generate a random number between 1 and 10 and displays
+
+function generateRandom() {
+    let numberDisplay = document.getElementById(“display”);
+    let currentNumber = Math.floor(Math.random() * 10) + 1;
+    let hiddenNumber = Math.floor(Math.random() * 10) + 1;  
+    console.log(randomNumber)
+    numberDisplay.innerHTML = randomNumber
+    }
+    
