a8979eee4fd8ce26de70799bb0da62ce8bb5494e
Tayla Ball <tayla.ball@hotmail.com>
2024-10-23 09:43:52 +0000

    Add: changes to generate random number in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,29 @@
 Let score = 0;
 
 // Generate a random number between 1 and 10 and displays
+document.getElementById("btn-start").document.addEventListener('click', generateRandom)
 
 function generateRandom() {
     let numberDisplay = document.getElementById(“display”);
     let currentNumber = Math.floor(Math.random() * 10) + 1;
     let hiddenNumber = Math.floor(Math.random() * 10) + 1;  
-    console.log(randomNumber)
-    numberDisplay.innerHTML = randomNumber
+    console.log(currentNumber)
+    numberDisplay.innerHTML = currentNumber
     }
+
+    // GuessedCorrectly message
+const guessedCorrectly = “Correct!”;
+
+// Guessed wrong message
+const guessedWrong = “Incorrect! Try again?”;
     
+// Higher 
+let isHigher = previous number >= previousNumber;
+let isLower = randomNumber <= previousNumber;
+
+if (userChoice === ‘higher’ && isHigher) || (userChoice === ‘lower’ && isLower); {
+return guessedCorrectly ;
+currentNumber += hiddenNumber
+Score ++
+} else { return guessedWrong;
+}
